549ebde55ff4edc8ccd27830370b43a5df336980
john@civifirst.com <john@civifirst.com>
2021-02-23 21:23:16 +0000

    Added skeleton tests for Transfer.

--- Moneybox.AppTests/Features/TransferMoneyTests.cs
+++ Moneybox.AppTests/Features/TransferMoneyTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Moneybox.AppTests.Features
+namespace Moneybox.App.Features.Tests
 {
-    class TransferMoneyTests
+    [TestClass()]
+    public class TransferMoneyTests
     {
+        [TestMethod()]
+        public void ExecuteNegativeAmountTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ExecuteInsufficientFundsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ExecuteWithdrawalLimitTest()
+        {
+
+
+        }
+        [TestMethod()]
+        public void ExecuteSuccessfulTransferTest()
+        {
+
+        }
     }
 }
